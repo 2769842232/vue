da05957dc99151fefe2356a73dd067875ec2f546
2769842232 <2769842232@qq.com>
2019-07-08 17:55:48 +0800

    更换武器 装备页面内容

--- src/components/foot.vue
+++ src/components/foot.vue
@@ -10,14 +10,14 @@
       <li>
         <a href="#">装备管理</a>
       </li>-->
-      <router-link class="active" to="/ying" tag="li">
-        <a>英雄列表</a>
-      </router-link>
-      <router-link to="/Zb" tag="li">
-        <a>武器列表</a>
+      <router-link to="/ying" tag="li">
+        <a>英雄管理</a>
       </router-link>
       <router-link to="/Wu" tag="li">
-        <a>装备列表</a>
+        <a>武器管理</a>
+      </router-link>
+      <router-link to="/equips" tag="li">
+        <a>装备管理</a>
       </router-link>
     </ul>
   </div>
--- src/router.js
+++ src/router.js
@@ -4,19 +4,20 @@ import VueRouter from 'vue-router'
 //配置router
 Vue.use(VueRouter)
 
-//导入英雄列表的组件
-import right from './views/Zb/right.vue'
-//引入武器列表
+//导入装备列表的组件
+import right from './views/equips/right.vue'
+//引入武器列表的组件
 import Wqi from './views/Wu/Wqi.vue'
 
 //创建路由对象
 const router = new VueRouter({
     linkActiveClass: 'active',
     routes: [
-        { name: 'home', path: '/', directives: '/Zb' },
-        { name: 'heros', path: '/Zb', component: right },
-        { name: 'wq', path: '/Wu', components: Wqi }
+        { name: 'one', path: '/', directives: 'Wqi' },
+        { name: 'wq', path: '/Wu', component: Wqi },
+        { name: 'Zb', path: '/equips', component: right }
     ]
+
 })
 
 //导出路由对象
